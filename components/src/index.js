804e35cca8f0354bc73ee23a4aa110dcb9cb61b0
Edwin Osorio <30908546+edwin1106@users.noreply.github.com>
2019-10-31 21:05:25 -0500

    Using external API to generate fake images

--- components/src/index.js
+++ components/src/index.js
@@ -1,8 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
-    return <div>Hola mundo</div>
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}></img>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date"> Hoy a las 9:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}></img>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date"> Hoy a las 9:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}></img>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date"> Hoy a las 9:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 ReactDOM.render(<App/>, document.querySelector('#root'))
