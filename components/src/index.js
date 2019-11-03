452f8d946aad33c0f7d365886947a12bca79f6ac
Edwin Osorio <30908546+edwin1106@users.noreply.github.com>
2019-11-03 15:42:59 -0500

    Adding ApprovalCard component

--- components/src/index.js
+++ components/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () =>{
     return(
         <div className="ui container comments">
+            <ApprovalCard/>
             <CommentDetail autor="Autor 1" fecha="Hoy a las 9:00Pm" image={faker.image.avatar()}/>
         </div>
     );
