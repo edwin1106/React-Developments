3905823056ec89e4262ece4e674806d1d2f237f2
Edwin Osorio <30908546+edwin1106@users.noreply.github.com>
2019-11-16 01:14:25 -0500

    Index file refactoring and seasonDisplay component customizing

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,37 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; 
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props){
-        super(props);
+  state = { lat: null, errorMessage: '' };
 
-        this.state = { lat: null, errorMessage: ''};
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
 
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                // Using setState!!!
-                this.setState({lat: position.coords.latitude});
-            },
-            err => {
-                this.setState({errorMessage: err.message})
-            }
-        );
+  render() {
+
+    if (this.state.lat && !this.state.errorMessage) {
+
+      return <SeasonDisplay lat= {this.state.lat}/>
     }
 
-    render(){
+    if (!this.state.lat && this.state.errorMessage) {
 
-        if(this.state.lat && !this.state.errorMessage){
-
-        return <div>Latitude: {this.state.lat}</div>
-        }
-
-        if(!this.state.lat && this.state.errorMessage){
-
-        return <div>Error: {this.state.errorMessage}</div>
-        }
-        return <div>Loading...</div>
-
-
+      return <div>Error: {this.state.errorMessage}</div>
     }
+    return <div>Loading...</div>
+
+
+  }
 }
-ReactDOM.render(<App/>,document.querySelector("#root"))
+ReactDOM.render(<App />, document.querySelector("#root"))
