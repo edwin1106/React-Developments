8413f07943e79666848ce88aa033778f3baa8655
edwinosoriogaviria <edwinosoriogaviria@hotmail.com>
2021-04-10 19:12:58 -0500

    Added hooks to use state in accordion component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import Accordion from './components/Accordion'
 
-export default () => {
-    return <div>Widgets App</div>
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a frontEnd javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library amon engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+]
+
+const App = () => {
+    return (
+        <div>
+            <Accordion items={items}/>
+        </div>
+    ); 
 };
+
+export default App;
---
+++ widgets/src/components/Accordion.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const Accordion = ({items}) => {
+
+    const [activeIndex,setActiveindex] = useState(null);
+
+    const onTitleClick = (index) => {
+        setActiveindex(index)
+    }
+
+    const renderedItems = items.map((item,index) => {
+        return (
+        <React.Fragment key={item.title}>
+            <div onClick={() => onTitleClick(index)} className="title active">
+                <i className="dropdown icon"></i>
+                {item.title}
+            </div>
+            <div className="content active">
+                <p>{item.content}</p>
+            </div>
+        </React.Fragment>
+        );
+
+    });
+    return (
+        <>
+        <div className="ui styled accordion">{renderedItems}</div>
+        <div>{activeIndex}</div>
+        </>
+
+    );
+};
+
+export default Accordion;
