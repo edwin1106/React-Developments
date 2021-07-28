5622f8b5f7ac2ae2aecd97704de83ddc7480a65d
edwinosoriogaviria <edwinosoriogaviria@hotmail.com>
2021-07-27 21:13:28 -0500

    Builded a configured dropdown component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion'
+import Dropdown from './components/Dropdown';
 import Search from './components/Search';
 
 const items = [
@@ -17,10 +18,30 @@ const items = [
     }
 ]
 
+const options = [
+    {
+        label: 'The color red',
+        value: 'red'
+    },
+    {
+        label: 'The color green',
+        value: 'green'
+    },
+    {
+        label: 'A shade of blue',
+        value: 'blue'
+    }
+]
+
 const App = () => {
+    const [selected, setSelected] = useState(options[0])
     return (
         <div>
-            <Search/>
+            <Dropdown 
+            selected={selected} 
+            onSelectedChange={setSelected}
+            options={options}
+            />
         </div>
     ); 
 };
