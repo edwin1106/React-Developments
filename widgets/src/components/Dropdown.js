072411b2d5d17ce8348974f14d188e25f836fdb4
edwinosoriogaviria <edwinosoriogaviria@hotmail.com>
2021-07-30 19:11:59 -0500

    Formated document and added svg to show dropdown value

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,27 +1,27 @@
 import React, { useEffect, useState, useRef } from "react";
 
-const Dropdown = ({options, selected, onSelectedChange}) => {
+const Dropdown = ({ options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false)
     const ref = useRef()
 
     useEffect(() => {
         const onBodyClick = (event) => {
-          if (ref.current.contains(event.target)) {
-            return;
-          }
-          setOpen(false);
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+            setOpen(false);
         };
         document.body.addEventListener("click", onBodyClick, { capture: true });
-     
+
         return () => {
-          document.body.removeEventListener("click", onBodyClick, {
-            capture: true,
-          });
+            document.body.removeEventListener("click", onBodyClick, {
+                capture: true,
+            });
         };
-      }, []);
-      
-    const renderedOptions = options.map((option)=>{
-        if(option.value === selected.value){
+    }, []);
+
+    const renderedOptions = options.map((option) => {
+        if (option.value === selected.value) {
             return null;
         }
         return (
@@ -30,11 +30,11 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
             </div>
         );
     });
-    return(
+    return (
         <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">Select a Color</label>
-                <div 
+                <div
                     onClick={() => setOpen(!open)}
                     className={`ui selection dropdown ${open ? `visible active` : ''}`}
                 >
@@ -42,6 +42,9 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
                     <div className="text">{selected.label}</div>
                     <div className={`menu ${open ? `visible transition` : ''}`}>{renderedOptions}</div>
                 </div>
+                <svg height="100" width="100">
+                    <circle cx="50" cy="50" r="40" stroke="black" stroke-width="2" fill={selected.value} />
+                </svg>
             </div>
         </div>
     )
